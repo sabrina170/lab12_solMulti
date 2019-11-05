3d6715fa5d6f6b70b3d47e0a748e5bff6b25e2ee
Sabrina Pomajulca <sabrina.pomajulca@tecsup.edu.pe>
2019-11-04 23:25:51 -0500

    first commit

---
+++ Lab12/Lab12/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab12
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.Data());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.Event());
+            };
+
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.Property());
+
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.Style());
+
+            };
+
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Lab12.MultiTrigger());
+
+            };
+
+
+        }
+    }
+}
